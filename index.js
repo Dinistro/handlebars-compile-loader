1308613dc87e9ea98fd03292e0ed69bdb1427675
Christian Ulmann <christian.ulmann@bit.admin.ch>
2017-02-28 13:23:54 +0100

    add helpers and partials to loader depenedencies

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 var handlebars = require('handlebars');
 var handlebarsWax = require('handlebars-wax');
 
-module.exports = function(source) {
+module.exports = function (source) {
     this.cacheable();
 
-    let options = this.query;
+    let addToDependencies = (option) => {
+        if (typeof option === 'string') {
+            this.dependency(option);
+        } else if (Array.isArray(option)) {
+            option.forEach((item) => {
+                this.dependency(item);
+            });
+        }
+    };
 
-    var hb = handlebars;//|| gulpHb.handlebars.create();
+    let options = this.query;
+    var hb = handlebars;
+
     var wax = handlebarsWax(hb, options);
 
     if (options.partials) {
-  		wax.partials(options.partials);
-  	}
+        wax.partials(options.partials);
 
-  	if (options.helpers) {
-  		wax.helpers(options.helpers);
-  	}
+        addToDependencies(options.partials);
+    }
 
-  	if (options.decorators) {
-  		wax.decorators(options.decorators);
-  	}
+    if (options.helpers) {
+        wax.helpers(options.helpers);
 
-  	if (options.data) {
-  		wax.data(options.data);
-  	}
+        addToDependencies(options.helpers);
+    }
+
+    if (options.decorators) {
+        wax.decorators(options.decorators);
+    }
+
+    if (options.data) {
+        wax.data(options.data);
+    }
 
     var template = wax.compile(source);
 
     return 'module.exports =' + JSON.stringify(template(options.data));
 };
+
